84fb306efe7434899b6354923f9b9711ce23bfba
Marco Pestrin <pestrinmarco@gmail.com>
2018-08-17 21:15:06 +0200

    sum dinamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,27 @@ import classes          from './Burger.css';
 import BurgerIngredient from './Burgeringredient/Burgeringredient';
 
 const burger = (props) => {
+    let newIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return  [...Array(props.ingredients[igKey])].map((_,i) => {
+                <BurgerIngredient key={igKey+i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) =>{
+            return arr.concat(el)
+        },[] )
+
+    console.log(newIngredients);
+
+    if(newIngredients.length === 0){
+        newIngredients = <p>Inizia ad aggiungere gli ingredienti</p>
+    }
+
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="bread-cheese"></BurgerIngredient>
-            <BurgerIngredient type="bread-meat"></BurgerIngredient>
-            <BurgerIngredient type="bread-bacon"></BurgerIngredient>
-            <BurgerIngredient type="bread-salad"></BurgerIngredient>
-            <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+            <BurgerIngredient type="bread-top" />
+                {newIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
